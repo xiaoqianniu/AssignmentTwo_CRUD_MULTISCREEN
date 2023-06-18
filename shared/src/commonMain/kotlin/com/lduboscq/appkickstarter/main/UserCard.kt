303caa13e3dd0420a10d4a180006e82f5ddc1803
Xiaowei Chen <xiaoqianniu@gmail.com>
2023-06-18 10:05:22 -0400

    version5: UserCard modified: adding two textfields to update password

--- shared/src/commonMain/kotlin/com/lduboscq/appkickstarter/main/UserCard.kt
+++ shared/src/commonMain/kotlin/com/lduboscq/appkickstarter/main/UserCard.kt
@@ -1,30 +1,103 @@
 package com.lduboscq.appkickstarter.main
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun UserCard(userData : UserData?){
+fun UserCard(userData: UserData?) {
+    var newPassword by remember { mutableStateOf("") }
+    var confirmNewPassword by remember { mutableStateOf("") }
+    var isEditing by remember { mutableStateOf(false) }
     Card(
-        modifier = Modifier.size(width = 180.dp, height = 100.dp)
-            .clip(MaterialTheme.shapes.small)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        elevation = 4.dp,
+        shape = MaterialTheme.shapes.medium
     ) {
-        if (userData != null) {
-            Column {
-                Text("UserName : " + userData?.username)
-                Text("Email : " + userData?.email)
-                Text("Password : " + userData?.password)
-                Text("Confirm Password : " + userData?.confirmPassword)
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                if (userData != null) {
+                    Text(
+                        text = "Username: ${userData.username}",
+                        style = MaterialTheme.typography.h6,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colors.primary
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = "Email: ${userData.email}")
+                    Text(text = "Password: ${userData.password}")
+                    Text(text = "Confirm Password: ${userData.confirmPassword}")
+
+                    if (isEditing) {
+                        Spacer(modifier = Modifier.height(16.dp))
+                        TextField(
+                            value = newPassword,
+                            onValueChange = { newPassword = it },
+                            label = { Text("New Password") },
+                            visualTransformation = PasswordVisualTransformation()
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        TextField(
+                            value = confirmNewPassword,
+                            onValueChange = { confirmNewPassword = it },
+                            label = { Text("Confirm New Password") },
+                            visualTransformation = PasswordVisualTransformation()
+                        )
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.End
+                        ) {
+                            TextButton(onClick = { /* Handle save click */ }) {
+                                Text("Save")
+                            }
+                            TextButton(onClick = { isEditing = false }) {
+                                Text("Cancel")
+                            }
+                        }
+                    }
+                } else {
+                    Text(
+                        text = "No User result",
+                        style = MaterialTheme.typography.body1,
+                        color = MaterialTheme.colors.error
+                    )
+                }
             }
-        } else {
-            Text("No User result")
+
+            if (!isEditing) {
+                TextButton(onClick = { isEditing = true }) {
+                    Text("Edit")
+                }
+            }
         }
     }
 }
